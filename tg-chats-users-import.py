bd8e20e9b04f6901fee471059d21b281a7c26cad
Roman Nikolaev <nikolaev.rd@yandex.ru>
2020-11-09 00:08:34 +0300

    Убрал лишние переносы строк

--- tg-chats-users-import.py
+++ tg-chats-users-import.py
@@ -183,9 +183,6 @@ else:
     log.info(f"✔ [IMPORT] Add all users to chat @{target_chat}")
 
 log.info(f"☻ [IMPORT] Users: {usernames_import}")
-client(InviteToChannelRequest(
-    target_chat,
-    usernames_import
-))
+client(InviteToChannelRequest(target_chat, usernames_import))
 
 log.info("⚑ Finish script")
